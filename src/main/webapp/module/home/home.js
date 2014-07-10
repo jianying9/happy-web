2a2183c1539b0a596ca809883e6fafd9609623e4
jiaying9 <jiaying9@192.168.1.101>
2014-07-10 21:35:51 +0800

    1、增加图片翻页 查询功能 2、增加页脚信息

--- src/main/webapp/module/home/home.js
+++ src/main/webapp/module/home/home.js
@@ -7,17 +7,48 @@ define(function(require) {
     var _module = require('yy/module');
     var self = {};
     var _event = _yy.getEvent();
+    var _message = _yy.getMessage();
     self.init = function(thisModule) {
+        //初始化图片列表
         var imageList = thisModule.findByKey('image-list');
+        var moreButton = thisModule.findByKey('more-button');
         imageList.init({
             key: 'id',
             itemClazz: '',
             itemDataToHtml: function(itemData) {
-                return '';
+                var result = '<div class="image_title">' + itemData.title + '</div>'
+                    + '<div class="image_wrap"><img alt="" src="' + itemData.mPicurl + '" /></div>'
+                    + '<div class="image_tools">'
+                    + '<div class="image_tool_item button"><div>踩</div><div class="label">(' + itemData.voteDown + ')</div></div>'
+                    + '<div class="image_tool_item button"><div>顶</div><div class="label">(' + itemData.voteUp + ')</div></div>'
+                    + '</div>';
+                return result;
             },
             itemCompleted: function(itemCom) {
             }
         });
+        _message.listen(imageList, 'INQUIRE_IMAGE_PAGE', function(thisCom, msg) {
+            if(msg.state === 'SUCCESS') {
+                imageList.loadData(msg.data.list);
+                imageList.setPageIndex(msg.data.pageIndex);
+                imageList.setPageSize(msg.data.pageSize);
+                if(msg.data.length === 0) {
+                    moreButton.hide();
+                }
+            }
+        });
+        //初始化更多按钮
+        _event.bind(moreButton, 'click', function(thisCom) {
+            var pageIndex = imageList.getPageIndex() + 1;
+            var pageSize = imageList.getPageSize();
+            _message.send({
+                act: 'INQUIRE_IMAGE_PAGE',
+                pageIndex: pageIndex,
+                pageSize: pageSize
+            });
+        });
+        //加载图片
+        _message.send({act: 'INQUIRE_IMAGE_PAGE', pageIndex: 1, pageSize: 10});
     };
     return self;
 });
