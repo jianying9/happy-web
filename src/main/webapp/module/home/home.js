ea588f484a6be8a92197e5415286db43f653603c
jianying9 <jianying9@jianying9>
2014-07-16 22:49:51 +0800

    1、优化微博分享

--- src/main/webapp/module/home/home.js
+++ src/main/webapp/module/home/home.js
@@ -13,26 +13,20 @@ define('home', ['require', 'yy/yy', 'yy/button', 'yy/list', 'weibo'], function(r
             itemDataToHtml: function(itemData) {
                 var result = '<div class="image_title">' + itemData.title + '</div>'
                         + '<div class="image_wrap"><img alt="" src="' + itemData.mPicurl + '" /></div>'
-                        + '<div class="image_tools">'
+                        + '<div class="image_tools skip">'
 //                        + '<div class="image_tool_item button"><div>踩</div><div class="label">(' + itemData.voteDown + ')</div></div>'
 //                        + '<div class="image_tool_item button"><div>顶</div><div class="label">(' + itemData.voteUp + ')</div></div>'
-                        + '<div class="image_tool_item"><div id="' + itemData.id + '-sina-publish" class="publishButton publishButton_red02"><span>发布到微博</span></div></div>'
+                        + '<div id="' + itemData.id + '-sina-publish" class="image_tool_item sina_publish button"></div>'
+                        + '<div class="image_tool_item_title">分享到:</div>'
                         + '</div>';
                 return result;
             },
             itemCompleted: function(itemCom) {
                 var data = itemCom.getData();
-                var publishId = data.id + '-sina-publish';
-                WB2.anyWhere(function(W) {
-                    W.widget.publish({
-                        id: publishId,
-                        default_text: '每天笑一笑，大脑清醒心脏好！http://www.bigcodebang.com',
-                        default_image: data.picurl,
-                        button_size: 'middle',
-                        button_type:'red',
-                        button_text:'发布到微博',
-                        toolbar: 'face,topic'
-                    });
+                var sinalPublishId = data.id + '-sina-publish';
+                var sinaPublishButton = itemCom.findChildByKey(sinalPublishId);
+                _event.bind(sinaPublishButton, 'click', function(thisCom) {
+                    window.open('http://service.weibo.com/share/share.php?title=' + encodeURIComponent(data.title) + '&url=' + encodeURIComponent("http://www.bigcodebang.com") + '&pic=' + encodeURIComponent(data.picurl) + '&appkey=238808965&searchPic=false', '_blank', "crollbars=no,width=600,height=450,left=75,top=20,status=no,resizable=no,location=no");
                 });
             }
         });
